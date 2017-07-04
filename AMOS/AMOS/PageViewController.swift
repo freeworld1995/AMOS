80f289d902d0d7cb9d29bab0f062590bb309efdb
Jimmy Hoang <freeworld3395@gmail.com>
2017-07-05 02:06:50 +0700

    fix

--- AMOS/AMOS/PageViewController.swift
+++ AMOS/AMOS/PageViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class PageViewController: UIPageViewController {
 
+    lazy var VCArr: [UIViewController] = {
+        return [UIStoryboard(name: "Main2", bundle: nil).instantiateViewController(withIdentifier: "controlCenter")]
+    }()
+    
+    // Input properties
+    var id: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,4 +28,10 @@ class PageViewController: UIPageViewController {
         // Dispose of any resources that can be recreated.
     }
 
+}
+
+extension PageViewController: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        <#code#>
+    }
 }

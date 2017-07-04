b69b98d441b8dc29e0e07afddf1ffd42255a210f
VanQuang <khang.quang113@gmail.com>
2017-07-05 02:06:18 +0700

    ghep code

--- AMOS/AMOS/ListAcountViewController.swift
+++ AMOS/AMOS/ListAcountViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ListAcountViewController: UIViewController , UITableViewDelegate, UITableViewDataSource{
     
@@ -14,6 +15,8 @@ class ListAcountViewController: UIViewController , UITableViewDelegate, UITableV
     
     @IBOutlet var addAcount: UIButton!
 
+    var devices : [Device]! = nil
+    let realm = try! Realm()
     @IBAction func addAcount(_ sender: Any) {
         
         let vc  = self.storyboard?.instantiateViewController(withIdentifier: "formacount") as! FormAcountViewController
@@ -21,7 +24,10 @@ class ListAcountViewController: UIViewController , UITableViewDelegate, UITableV
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let result = realm.objects(Device.self)
+        self.devices = Array(result)
+        
         addAcount.layer.cornerRadius = 10;
         addAcount.layer.borderWidth = 2;
         addAcount.layer.borderColor = UIColor.black.cgColor
@@ -32,7 +38,12 @@ class ListAcountViewController: UIViewController , UITableViewDelegate, UITableV
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        let result = realm.objects(Device.self)
+        self.devices = Array(result)
+        self.tableView.reloadData()
+    }
 
 
 }
@@ -42,18 +53,24 @@ extension ListAcountViewController{
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10;
+        return devices.count;
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellistacount", for: indexPath)
-        cell.textLabel?.text = "aaaaa"
+        cell.textLabel?.text = devices[indexPath.row].name
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         // didselec
+        
+        let story = UIStoryboard.init(name: "Main2", bundle: nil)
+        let vc = story.instantiateViewController(withIdentifier: "page") as! ControlCenterViewController
+        vc.id = devices[indexPath.row].id
+        
+        self.navigationController?.pushViewController(vc, animated: true)
         
     }
     
@@ -65,10 +82,8 @@ extension ListAcountViewController{
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             print("Deleted")
-            
-            // array Acount --
-            
-//            yourArray.remove(at: indexPath.row)
+            RealmManager.deleteDevice(id: devices[indexPath.row].id)
+            devices.remove(at: indexPath.row)
             tableView.reloadData()
         }
     }

6671e8bb6b4e2fecb86ce1dfc9ba233ade0ba93a
Jimmy Hoang <freeworld3395@gmail.com>
2017-07-15 21:15:23 +0700

    fix change password realm

--- AMOS IOS/AMOS/AppDelegate.swift
+++ AMOS IOS/AMOS/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import IQKeyboardManagerSwift
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,6 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        print(Realm.Configuration.defaultConfiguration.fileURL?.absoluteURL)
         
         IQKeyboardManager.sharedManager().enable = true
         
